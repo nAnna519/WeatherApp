df2d08577a690cb0057ecbed2b2415023d36eae4
Ann519 <anna10anna10@yandex.ru>
2019-05-09 16:20:49 +0300

    Added getting information from api request

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.Charfield(max_length=30)
--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,24 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    app_id = 'c0d78490bfcbfdc9dd77405dc48245d0'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + app_id
+    city = 'London'
+
+    response = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': response['main']['temp'],
+        'icon': response['weather'][0]['icon']
+    }
+
+    context = {
+        'info': city_info
+    }
+
+    return render(request, 'weather/index.html', context)
